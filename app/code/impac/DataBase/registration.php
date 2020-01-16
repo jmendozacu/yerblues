e18126b032db24bf09b817a9a1c50176c7439c71
elarayax <byron.aros@gmail.com>
2020-01-16 16:11:36 -0300

    14

--- app/code/impac/DataBase/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-use \Magento\Framework\Component\ComponentRegistrar;
-\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE,"DataBaseAlpha3",__DIR__);
-//ComponentRegistrar::register(ComponentRegistrar::MODULE, 'DataBaseAlpha3', __DIR__);
